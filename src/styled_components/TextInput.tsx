b977c973044c156e9d5b42faac805f2c165b9434
Allie Soldau <alliesoldau@gmail.com>
2024-07-30 09:58:35 -0400

    fixed up padding for text inputs

--- src/styled_components/TextInput.tsx
+++ src/styled_components/TextInput.tsx
@@ -4,14 +4,19 @@ import styled from '@emotion/styled';
 const TextInput = styled.input<{ error?: boolean }>`
   background-color: transparent;
   border: 1px solid transparent;
+  font-size: 1.25rem;
   border-bottom: 1px solid ${props => props.error ? 'red' : '#9E9E9E'};
   height: 1.5rem;
+  padding: 0.5rem 0;
   :hover {
     border-bottom: 1px solid ${props => props.error ? 'red' : '#fff'};
   }
-  transition: border-color 0.4s ease-in-out;
+  transition: border-color 0.4s ease-in-out, padding 0.4s ease-in-out;
   color: #fff;
-  padding: 0 1rem;
+  &:focus {
+    padding: 1rem 0;
+    outline: none;
+  }
 `;
 
 export default TextInput;
