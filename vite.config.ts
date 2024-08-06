3214bbc427e87e85630fcae6575fd5bb2a063fcd
hkwadam <87677738+hkwadam@users.noreply.github.com>
2024-08-06 11:53:57 -0700

    change base to ./ (current directory)
    changing the base from /hkw-2024/ to ./ to create links in the proper locations for deployment to vercel.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // remove base and run build if pushing to bluehost -- grab files from dist 
-  base: "/hkw-2024/",
+  base: "./",
   plugins: [react()],
 })
